17ed26e2ac5376984c158ccc401f6020c11ee89e
ismilearefin <arefinismile@gmail.com>
2023-02-03 01:57:07 +0600

    Text Section Design

---
+++ components/TextSection/TextSection.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const TextSection = () => {
+    return (
+        <div className='flex items-center justify-evenly my-[120px]'>
+            <div className=''>
+                <h1 className='text-[46px] font-[Grotesque] text-[#262524]'>Healthy in side<br/> fresh out side</h1>
+            </div>
+            <div className='w-[674px]'>
+                <p className='text-base'>Exercise is a very important need for our body. Health and fitness will be obtained if you can do regular exercise and run a healthy routine. Even at home we still have to be diligent in exercising, either alone or with your friends at home. Healthy life makes you more excited to live the day</p>
+            </div>
+        </div>
+    );
+};
+
+export default TextSection;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import HeroSection from "components/HeroSection/HeroSection";
+import TextSection from "components/TextSection/TextSection";
 
 
 export default function Home() {
   return (
     <main className=" w-[1440px] mx-auto">
       <HeroSection></HeroSection>
+      <TextSection></TextSection>
     </main>
   )
 }
